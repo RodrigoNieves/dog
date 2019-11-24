81935999909ff28bf9e1af5d228d357543533ddd
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-23 17:19:49 -0800

    Setting Eureka Client

--- src/main/java/me/rrsn/dog/DogApplication.java
+++ src/main/java/me/rrsn/dog/DogApplication.java
@@ -2,8 +2,10 @@ package me.rrsn.dog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DogApplication {
 
 	public static void main(String[] args) {
